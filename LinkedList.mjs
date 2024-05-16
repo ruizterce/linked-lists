51d52cfdff740ad4b7ddbb6091dc964d4a8244e5
ruiz.terce <ruiz.terce@gmail.com>
2024-05-16 18:49:15 +0200

    Add functions to LinkedList
    Add contains(value), find (value) and toString()
    functions to LinkedList
    
    Add messages in console to explain what's going
    on during execution in index.mjs

--- LinkedList.mjs
+++ LinkedList.mjs
@@ -5,6 +5,7 @@ export default class LinkedList {
     this.headNode = headNode;
   }
 
+  // Add a new node containing value to the end of the list
   append(value) {
     if (this.headNode === null) {
       this.headNode = new Node(value, null);
@@ -17,11 +18,11 @@ export default class LinkedList {
 
     pointer.nextNode = new Node(value, null);
   }
-
+  // Add a new node containing value to the start of the list
   prepend(value) {
     this.headNode = new Node(value, this.head);
   }
-
+  // Return the total number of nodes in the list
   get size() {
     if (this.headNode === null) {
       return 0;
@@ -34,11 +35,11 @@ export default class LinkedList {
     }
     return size;
   }
-
+  // Return the first node in the list
   get head() {
     return this.headNode;
   }
-
+  // Return the last node in the list
   get tail() {
     if (this.headNode === null) {
       return null;
@@ -49,7 +50,7 @@ export default class LinkedList {
     }
     return pointer;
   }
-
+  // Return the node at the given index (head = 0)
   at(index) {
     if (index < 0 || !Number.isInteger(index)) {
       return null;
@@ -63,7 +64,7 @@ export default class LinkedList {
     }
     return pointer;
   }
-
+  // Remove the last element from the list
   pop() {
     if (this.headNode === null) {
       return;
@@ -76,5 +77,37 @@ export default class LinkedList {
     }
     pointer.value = null;
     previousNode.nextNode = null;
+  }
+  // Returns true if the passed in value is in the list and otherwise returns false
+  contains(value) {
+    let pointer = this.headNode;
+    while (pointer !== null) {
+      if (pointer.value === value) return true;
+      pointer = pointer.nextNode;
+    }
+    return false;
+  }
+  // Return the index of the node containing value, or null if not found (head = 0)
+  find(value) {
+    let pointer = this.headNode;
+    let index = 0;
+    while (pointer !== null) {
+      if (pointer.value === value) return index;
+      index += 1;
+      pointer = pointer.nextNode;
+    }
+    return null;
+  }
+
+  // Represent LinkedList object as a string to visualize the structure
+  toString() {
+    let string = "";
+    let pointer = this.headNode;
+    while (pointer != null) {
+      string += "( " + pointer.value + " ) -> ";
+      pointer = pointer.nextNode;
+    }
+    string += "null";
+    return string;
   }
 }
--- index.mjs
+++ index.mjs
@@ -6,17 +6,22 @@ myList.append("Appended value");
 myList.append("Another appended value");
 myList.prepend("Prepended value");
 
-console.log(myList);
-console.log(myList.size);
+console.log("Initial list:");
+console.log(myList.toString());
+console.log("size = " + myList.size);
 console.log("Head Node:");
 console.log(myList.head);
 console.log("Tail Node:");
 console.log(myList.tail);
 console.log("Third Node:");
 console.log(myList.at(2));
+console.log("Popping tail Node...");
 myList.pop();
+console.log("Updated list:");
+console.log(myList.toString());
 console.log("New Tail Node:");
 console.log(myList.tail);
-console.log(myList);
-
-console.log("ok");
+console.log("list contains: 'Appended value'?");
+console.log(myList.contains("Appended value"));
+console.log("Index of: 'Appended value'? (head = 0)");
+console.log(myList.find("Appended value"));
